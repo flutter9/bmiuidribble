381908c2520d808b696aadbdd64ecd2ac11d2c89
InspireUI <minhcasi@gmail.com>
2020-03-29 09:19:39 +0700

    build web

---
+++ flutter_service_worker.js
@@ -0,0 +1,30 @@
+'use strict';
+const CACHE_NAME = 'flutter-app-cache';
+const RESOURCES = {
+  "/index.html": "e6e25ef07ab061f3396db68372e4cc59",
+"/main.dart.js": "67f30e0b93fbff8eb74de7a9d6e1259e"
+};
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheName) {
+      return caches.delete(cacheName);
+    }).then(function (_) {
+      return caches.open(CACHE_NAME);
+    }).then(function (cache) {
+      return cache.addAll(Object.keys(RESOURCES));
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
